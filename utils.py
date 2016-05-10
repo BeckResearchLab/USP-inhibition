192b9c7f3e29292d7a2dfe4d21c783d69c0a0547
Pearl Philip <pearlphilip007@gmail.com>
2016-05-10 12:37:10 -0700

    Added utilities file

---
+++ utils.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+
+def create_dict(filename, mol):
+    """
+    Returns dictionary of sample ID and molecular notation
+    
+    Inputs: filename, mol
+    filename - path to file containing molecular notation indexed by sample ID
+    mol - type of molecular notation
+    Input types: str, str
+    
+    Outputs: dictionary of molecular notation indexed by sample ID
+    Output types: Python dictionary
+    """
+    with open(filename, 'r') as f:
+        # Reads the file line by line
+        data = f.readlines()
+        # Null dictionary
+        df = (dict([]))
+        for line in data[30000:30001]:
+            # Splits the line into it's key and molecular string  
+            words = line.split()
+            if mol == 'smiles':
+                z = (dict([(int(words[0]), [words[1]])]))
+            elif mol == 'inchi':
+                # This removes the 'InChI=' prefix to the InChI string
+                z = (dict([(int(words[0]), [words[1][6:]])]))
+            else:
+                print 'Invalid molecular notation. Choose from smiles or inchi.'
+            # Appending dictionary            
+            df.update(z)
+        return df
+
+
+def create_dataframe(filename, mol):
+    """
+    Returns Pandas dataframe of sample ID and molecular notation
+    
+    Inputs: filename, mol
+    filename - path to file containing molecular notation indexed by sample ID
+    mol - type of molecular notation
+    Input types: str, str
+    
+    Outputs: dataframe of molecular notation indexed by sample ID
+    Output types: Pandas DataFrame
+    """
+    with open(filename, 'r') as f:
+        # Reads the file line by line
+        data = f.readlines()
+        # Null dataframe
+        df = []
+        for line in data[130000:130500]:
+            # Splits the line into it's key and molecular string  
+            words = line.split()
+            if mol == 'smiles':
+                z = [int(words[0]), words[1]]
+            elif mol == 'inchi':
+                # This removes the 'InChI=' prefix to the InChI string
+                z = [int(words[0]), words[1][6:]]
+            else:
+                print 'Invalid molecular notation. Choose from smiles or inchi.'
+            # Appending dictionary            
+            df.append(z)
+        df = pd.DataFrame(df)
+        df.columns = ['ID', mol.upper()]
+        return df
