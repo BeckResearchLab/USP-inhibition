5ac1dce80d0bfe4c52a2de5de4beefe235b8ad66
Pearl Philip <pearlphilip007@gmail.com>
2016-07-01 09:54:31 -0700

    Load SVM pickle and print metrics

--- post_process.py
+++ post_process.py
@@ -15,10 +15,12 @@ from nolearn.lasagne import NeuralNet
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
 
-PICKLE = 'data.pkl'
+NN_PICKLE = 'nn_data.pkl'
+SVM_PICKLE = 'svm_data.pkl'
+
 
 def results():
-    with open(PICKLE, 'rb') as file:
+    with open(NN_PICKLE, 'rb') as file:
         grid_search = pickle.load(file)
         net = pickle.load(file)
 
@@ -26,4 +28,13 @@ def results():
     print(grid_search.best_estimator_)
     print(grid_search.best_score_)
     print(grid_search.best_params_)
+    grid_search.save_params_to('/tmp/grid_search.params')
     net.save_params_to('/tmp/net.params')
+
+    with open(SVM_PICKLE, 'rb') as file:
+        mean_abs = pickle.load(file)
+        mean_sq = pickle.load(file)
+        median_abs = pickle.load(file)
+        r2 = pickle.load(file)
+
+    print mean_abs, mean_sq, median_abs, r2
