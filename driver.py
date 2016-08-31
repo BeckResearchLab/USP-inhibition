4bfe2b3e077d98a57ad75a445adb2ea1d69d0e6c
Pearl Philip <pearlphilip007@gmail.com>
2016-08-31 15:07:33 -0700

    Removed inchi data from working dataframe to decrease size

--- driver.py
+++ driver.py
@@ -11,7 +11,7 @@ from sklearn.cross_validation import train_test_split
 TARGET_COLUMN = 'Activity_Score'
 
 # To find the number of compounds tested; expected 389561
-with open('data/chemical_notation_data/compounds_inchi.txt', 'r') as f:
+with open('data/chemical_notation_data/compounds_smiles.txt', 'r') as f:
     data = f.readlines()
     i = 1
     for line in data:
@@ -27,39 +27,19 @@ def main():
     @:param: None
     :return: Post process results
     """
+    # Importing inhibitor notation data
     # The SMILES and InChI logs of the same material have identical indices
     # Creating and joining the SMILES and InChI dataframes along the same index
 
     df_compounds_smiles = utils.create_dataframe('data/chemical_notation_data/'
                                                  'compounds_smiles.txt', 'smiles')
-    df_compounds_inchi = utils.create_dataframe('data/chemical_notation_data/'
-                                                'compounds_inchi.txt', 'inchi')
-
-    df_compounds = pd.concat([df_compounds_smiles, df_compounds_inchi['INCHI']],
-                             axis=1).rename(columns={'ID': 'CID'})
+    df_compounds = df_compounds_smiles.rename(columns={'ID': 'CID'})
+    df_compounds = df_compounds.sort_values(by='CID')
 
     # Importing inhibitor activity data
     activity = pd.read_csv('data/activity_data/AID_743255_datatable.csv')
-    # Eliminates first five text rows of csv
-    for j in range(5):
-        activity = activity.drop(j, axis=0)
-    activity = activity.drop(['PUBCHEM_ACTIVITY_URL', 'PUBCHEM_RESULT_TAG',
-                              'PUBCHEM_ACTIVITY_SCORE', 'PUBCHEM_SID',
-                              'PUBCHEM_ASSAYDATA_COMMENT', 'Potency',
-                              'Efficacy', 'Analysis Comment',
-                              'Curve_Description', 'Fit_LogAC50',
-                              'Fit_HillSlope', 'Fit_R2', 'Fit_InfiniteActivity',
-                              'Fit_ZeroActivity', 'Fit_CurveClass',
-                              'Excluded_Points', 'Compound QC', 'Max_Response',
-                              'Activity at 0.457 uM', 'Activity at 2.290 uM',
-                              'Activity at 11.40 uM', 'Activity at 57.10 uM',
-                              'PUBCHEM_ACTIVITY_OUTCOME'], axis=1)
-    activity.rename(columns={'PUBCHEM_CID': 'CID'}, inplace=True)
-    # Eliminates duplicate compound rows
-    activity['dupes'] = activity.duplicated('CID')
-    activity = activity[activity['dupes'] == 0].drop(['dupes'], axis=1)
-    df_compounds = df_compounds.sort_values(by='CID')
-    activity = activity.sort_values(by='CID')
+    activity = utils.clean_activity_dataframe(activity)
+
     # Merging activity data and compound notation data
     df = activity.merge(df_compounds)
     df = df.sort_values(by='CID')
@@ -69,7 +49,7 @@ def main():
     # Transform all column values to mean 0 and unit variance
     df_descriptor = utils.transform_dataframe(df_descriptor)
     # Drop non-descriptor columns before training
-    df = df.drop(['SMILES', 'INCHI', 'CID',	'Phenotype'], axis=1)
+    df = df.drop(['SMILES', 'CID',	'Phenotype'], axis=1)
     df = df.join(df_descriptor)
     # Feature selection and reduction
     # Data to training task
