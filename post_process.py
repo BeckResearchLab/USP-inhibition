2464265aa6a96564ac4a8ec3ad27e142e3edaf31
Pearl Philip <pearlphilip007@gmail.com>
2016-10-03 09:41:12 -0700

    Modified file shadow name, added print statements for each metric, read exp_var_score from pickle and print it

--- post_process.py
+++ post_process.py
@@ -20,21 +20,35 @@ SVM_PICKLE = 'svm_data.pkl'
 
 
 def results():
-    with open(NN_PICKLE, 'rb') as file:
-        grid_search = pickle.load(file)
-        net = pickle.load(file)
+    with open(NN_PICKLE, 'rb') as result:
+        grid = pickle.load(result)
+        net = pickle.load(result)
+        accuracy = pickle.load(result)
 
-    print(grid_search.grid_scores_)
-    print(grid_search.best_estimator_)
-    print(grid_search.best_score_)
-    print(grid_search.best_params_)
-    grid_search.save_params_to('/tmp/grid_search.params')
+    print(grid.grid_scores_)
+    print(grid.best_estimator_)
+    print(grid.best_score_)
+    print("Best parameters set found on development set:")
+    print(grid.best_params_)
+    print("Accuracy prediction score:")
+    print(accuracy)
+    grid.save_params_to('/tmp/grid.params')
     net.save_params_to('/tmp/net.params')
 
-    with open(SVM_PICKLE, 'rb') as file:
-        mean_abs = pickle.load(file)
-        mean_sq = pickle.load(file)
-        median_abs = pickle.load(file)
-        r2 = pickle.load(file)
+    with open(SVM_PICKLE, 'rb') as result:
+        mean_abs = pickle.load(result)
+        mean_sq = pickle.load(result)
+        median_abs = pickle.load(result)
+        r2 = pickle.load(result)
+        exp_var_score = pickle.load(result)
 
-    print mean_abs, mean_sq, median_abs, r2
+    print("Mean absolute error regression loss:")
+    print mean_abs
+    print("Mean squared error regression loss:")
+    print mean_sq
+    print("Median absolute error regression loss:")
+    print median_abs
+    print("R^2 (coefficient of determination) regression score function:")
+    print r2
+    print("Explained variance regression score function:")
+    print exp_var_score
