105494f885d5358a518de9c0e10582c9ac620c9f
Pearl Philip <pearlphilip007@gmail.com>
2017-02-16 05:44:27 -0800

    Added functional version of genetic algorithm

--- genalgo.py
+++ genalgo.py
@@ -7,6 +7,8 @@ activity and returns the characteristics of the parent drug compound.
 
 from random import randint, random
 from operator import add
+import pandas as pd
+import pickle
 
 __author__ = "Pearl Philip"
 __credits__ = "David Beck"
@@ -15,42 +17,113 @@ __maintainer__ = "Pearl Philip"
 __email__ = "pphilip@uw.edu"
 __status__ = "Development"
 
+PICKLE = 'svm_data.pkl'
 
-def find_parents(n_rows, n_columns):
 
-    # [Init pop, mutation rate, # generations, chromosome/solution length, # winners/per gen]
-    initpop, mutrate, numgen, sol_len, num_win = n_rows, 0.1, 50, n_columns, 0.1 * n_rows
-    # Initialize current population to random values within range
-    curpop = np.random.choice(np.arange(-15, 15, step=0.01), size=(initpop, sol_len), replace=False)
-    nextpop = np.zeros((curpop.shape[0], curpop.shape[1]))
-    fitvec = np.zeros((initpop, 2))  # 1st col is indices, 2nd col is cost
-    for i in range(numgen):  # Iterate through # generations
-        # Create vector of all errors from cost function for each solution
-        fitvec = np.array([np.array([x, np.sum(NN.costFunction(NN.X, NN.y, curpop[x].T))]) for x in range(initpop)])
-        print("(Gen: #%s) Total error: %s\n" % (i, np.sum(fitvec[:, 1])))
-        plt.pyplot.scatter(i, np.sum(fitvec[:, 1]))
-        winners = np.zeros((num_win, sol_len))  # 10x3
-        for n in range(len(winners)):  # For n in range(10)
-            selected = np.random.choice(range(len(fitvec)), num_win/2, replace=False)
-            wnr = np.argmin(fitvec[selected, 1])
-            winners[n] = curpop[int(fitvec[selected[wnr]][0])]
-        nextpop[:len(winners)] = winners  # Populate new gen with winners
-        duplic_win = np.zeros(((initpop - len(winners)), winners.shape[1]))
-        for x in range(winners.shape[1]):  # For each col in winners (3 cols)
-            # Duplicate winners (10x3 matrix) 9 times to create a 90x3 matrix, then shuffle columns
-            num_dups = ((initpop - len(winners))/len(winners))  # Num times to duplicate, needs to fill rest of nextpop
-            duplic_win[:, x] = np.repeat(winners[:, x], num_dups, axis=0)  # Duplicate each col
-            duplic_win[:, x] = np.random.permutation(duplic_win[:, x])  # Shuffle each col ("crossover")
-        # Populate the rest of the generation with offspring of mating pairs
-        nextpop[len(winners):] = np.matrix(duplic_win)
-        # Create a mutation matrix, mostly 1s, but some elements are random numbers from a normal distribution
-        mutmatrix = [np.float(np.random.normal(0, 2, 1)) if rn.random() < mutrate else 1 for x in range(nextpop.size)]
-        # Randomly mutate part of the population by multiplying nextpop by our mutation matrix
-        nextpop = np.multiply(nextpop, np.matrix(mutmatrix).reshape(nextpop.shape))
-        curpop = nextpop
-    plt.pyplot.ylabel('Total cost/err')
-    plt.pyplot.xlabel('Generation #')
-    parent = curpop[np.argmin(fitvec[:, 1])]
+def main():
+    target = 100
+    p_count = 1000
+    i_length = 10
+    i_min = -2
+    i_max = 2
+    p = population(p_count, i_length, i_min, i_max)
+    parents = evolve(p, target)
+    print parents
 
-    return parent
 
+def individual(length, minimum, maximum):
+    """
+    Create a member of the population.
+    :param length:
+    :param minimum:
+    :param maximum:
+    :return:
+    """
+    return [randint(minimum, maximum) for x in xrange(length)]
+
+
+def population(count, length, minimum, maximum):
+    """
+    Create a number of individuals (i.e. a population).
+
+    :param count: the number of individuals in the population
+    :param length: the number of values per individual
+    :param minimum: the minimum possible value in an individual's list of values
+    :param maximum: the maximum possible value in an individual's list of values
+
+    """
+    return [individual(length, minimum, maximum) for x in xrange(count)]
+
+
+def fitness(individuals, target):
+    """
+    Determine the fitness of an individual. Higher is better.
+
+    :param individuals: the individual to evaluate
+    :param target: the target number individuals are aiming for
+    """
+    with open(PICKLE, 'rb') as result:
+        clf = pickle.load(result)
+    x_test = pd.DataFrame(individuals)
+    activity = clf.predict(x_test)
+    return abs(target - activity)
+
+
+def grade(pop, target):
+    """
+    Find average fitness for a population.
+    :param pop:
+    :param target:
+    :return:
+    """
+    summed = reduce(add, (fitness(x, target) for x in pop))
+    return summed / (len(pop) * 1.0)
+
+
+def evolve(pop, target, retain=0.2, random_select=0.05, mutate=0.01):
+    """
+
+    :param pop:
+    :param target:
+    :param retain:
+    :param random_select:
+    :param mutate:
+    :return: parents
+    """
+    graded = [(fitness(x, target), x) for x in pop]
+    graded = [x[1] for x in sorted(graded)]
+    retain_length = int(len(graded)*retain)
+    parents = graded[:retain_length]
+    # randomly add other individuals to
+    # promote genetic diversity
+    for individuals in graded[retain_length:]:
+        if random_select > random():
+            parents.append(individuals)
+    # mutate some individuals
+    for individuals in parents:
+        if mutate > random():
+            pos_to_mutate = randint(0, len(individuals)-1)
+            # this mutation is not ideal, because it
+            # restricts the range of possible values,
+            # but the function is unaware of the min/max
+            # values used to create the individuals,
+            individuals[pos_to_mutate] = randint(
+                min(individuals), max(individuals))
+    # crossover parents to create children
+    parents_length = len(parents)
+    desired_length = len(pop) - parents_length
+    children = []
+    while len(children) < desired_length:
+        male = randint(0, parents_length-1)
+        female = randint(0, parents_length-1)
+        if male != female:
+            male = parents[male]
+            female = parents[female]
+            half = len(male) / 2
+            child = male[:half] + female[half:]
+            children.append(child)
+    parents.extend(children)
+    return parents
+
+if __name__ == "__main__":
+    main()
