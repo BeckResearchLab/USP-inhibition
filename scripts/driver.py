1180d7c29e144bbb1f0b6e45d1df0888dfa179ba
Pearl Philip <pearlphilip007@gmail.com>
2017-04-04 08:11:41 -0700

    Added S3 uploads from driver.py for generated dataframes stored locally

--- scripts/driver.py
+++ scripts/driver.py
@@ -29,6 +29,10 @@ __status__ = "Development"
 
 TARGET_COLUMN = 'Activity_Score'
 XY_PICKLE = '../data/xy_data.pkl'
+access_keys = pd.read_csv('../keys/accessKeys.csv')
+AWS_ACCESS_KEY_ID = access_keys['Access key ID'][0]
+AWS_ACCESS_SECRET_KEY = access_keys['Secret access key'][0]
+BUCKET = 'pphilip-usp-inhibition'
 
 
 def main():
@@ -71,11 +75,26 @@ def main():
             df_x = utils.join_dataframes()
 
             df_x.to_csv('../data/df_x_preprocessing.csv')
+            file_to_s3 = open('../data/df_x_preprocessing.csv', 'r+')
+            key = file_to_s3.name
+            if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+                print("df_x_preprocessing has been uploaded to S3")
+            else:
+                print("df_x_preprocessing could not be uploaded to S3")
+
             df_y.to_csv('../data/df_y_preprocessing.csv')
+            file_to_s3 = open('../data/df_y_preprocessing.csv', 'r+')
+            key = file_to_s3.name
+            if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+                print("df_y_preprocessing has been uploaded to S3")
+            else:
+                print("df_y_preprocessing could not be uploaded to S3")
 
         else:
-            df_x = pd.read_csv('../data/df_x_preprocessing.csv')
-            df_y = pd.read_csv('../data/df_y_preprocessing.csv')
+            df_x = pd.read_csv('https://s3-us-west-2.amazonaws.com/pphilip-usp-inhibition/'
+                               'data/df_x_preprocessing.csv')
+            df_y = pd.read_csv('https://s3-us-west-2.amazonaws.com/pphilip-usp-inhibition/'
+                               'data/df_y_preprocessing.csv')
             df_x.drop(df_x.columns[0], axis=1, inplace=True)
             df_y.drop(df_y.columns[0], axis=1, inplace=True)
 
@@ -94,12 +113,35 @@ def main():
         df_y = pd.DataFrame(df_y)
         coefficients = pd.DataFrame({'existence': coefficients, 'column names': headers})
         df_x.to_csv('../data/df_x_postprocessing.csv')
+        file_to_s3 = open('../data/df_x_postprocessing.csv', 'r+')
+        key = file_to_s3.name
+        if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+            print("df_x_postprocessing has been uploaded to S3")
+        else:
+            print("df_x_postprocessing could not be uploaded to S3")
+
         df_y.to_csv('../data/df_y_postprocessing.csv')
+        file_to_s3 = open('../data/df_y_postprocessing.csv', 'r+')
+        key = file_to_s3.name
+        if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+            print("df_y_postprocessing has been uploaded to S3")
+        else:
+            print("df_y_postprocessing could not be uploaded to S3")
+
         coefficients.to_csv('../data/feature_coefficients.csv')
+        file_to_s3 = open('../data/feature_coefficients.csv', 'r+')
+        key = file_to_s3.name
+        if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+            print("feature_coefficients has been uploaded to S3")
+        else:
+            print("feature_coefficients could not be uploaded to S3")
     else:
-        df_x = pd.read_csv('../data/df_x_postprocessing.csv')
-        df_y = pd.read_csv('../data/df_y_postprocessing.csv')
-        coefficients = pd.read_csv('../data/feature_coefficients.csv')
+        df_x = pd.read_csv('https://s3-us-west-2.amazonaws.com/pphilip-usp-inhibition/'
+                           'data/df_x_postprocessing.csv')
+        df_y = pd.read_csv('https://s3-us-west-2.amazonaws.com/pphilip-usp-inhibition/'
+                           'data/df_y_postprocessing.csv')
+        coefficients = pd.read_csv('https://s3-us-west-2.amazonaws.com/pphilip-usp-inhibition/'
+                                   'data/feature_coefficients.csv')
         df_x.drop(df_x.columns[0], axis=1, inplace=True)
         df_y.drop(df_y.columns[0], axis=1, inplace=True)
         coefficients.drop(coefficients.columns[0], axis=1, inplace=True)
@@ -107,6 +149,8 @@ def main():
     coefficients['existence'] = coefficients['existence'].astype(int)
     df_x.columns = list(coefficients[coefficients['existence'] == 1]['column names'])
     df_y.columns = ['Activity_Score']
+
+    utils.plot_features(df_x, df_y)
     df = df_x.join(df_y)
 
     # Data to training task
@@ -142,6 +186,13 @@ def main():
         pickle.dump(x_test, results, pickle.HIGHEST_PROTOCOL)
         pickle.dump(y_train, results, pickle.HIGHEST_PROTOCOL)
         pickle.dump(y_test, results, pickle.HIGHEST_PROTOCOL)
+
+    file_to_s3 = open('../data/xy_data.pkl', 'r+')
+    key = file_to_s3.name
+    if utils.upload_to_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, file_to_s3, BUCKET, key):
+        print("xy_pickle has been uploaded to S3")
+    else:
+        print("xy_pickle could not be uploaded to S3")
 
     print("Generating models")
     models.run_models(x_train, y_train, x_test, y_test)
